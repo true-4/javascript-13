1aa9a3c9419f3b3e3862dfd82ab110a5b33c291d
kuga <esixterb58@gmail.com>
2025-06-20 13:33:10 +0300

    feat: task-3

--- index.js
+++ index.js
@@ -56,10 +56,31 @@ console.log(adminFirst.getInfo())  // невнимателен был испра
 // Баланс можно узнать только через метод
 
 class BankAccount {
-  #balance
   constructor(balance) {
-    this.#balance = balance
+    this._balance = balance
   }
 
+  deposit(amount) {
+    this._balance += amount
+    return 'Баланс обнулён'
+  }
 
+  withdraw(amount){
+    if (this._balance >= amount) {
+      return this._balance -= amount
+    } else {
+      throw 'Недостаточно средств'
+    }
+  }
+
+  getBalance() {
+    return this._balance
+  }
 }
+
+const myAccount = new BankAccount(174900)
+console.log(myAccount)
+console.log(myAccount.getBalance())
+console.log(myAccount.withdraw(4900))
+console.log(myAccount)
+console.log(myAccount.deposit())
